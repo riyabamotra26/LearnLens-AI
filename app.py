ec6f747d31522b8ab32507e78ebaf3a0e81938fd
riya <riyabamotra26@gmail.com>
2026-09-08 19:51:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask import (
 from werkzeug.security import check_password_hash, generate_password_hash
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=".", static_folder=".")
 app.secret_key = "learnlens-secret-key"
 
 DATABASE = "learnlens.db"
